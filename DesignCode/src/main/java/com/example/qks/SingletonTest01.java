0e1b649635a168b3789df5d192d67c578e919b96
senluoye <57495136+senluoye@users.noreply.github.com>
2022-01-30 15:22:07 +0800

    增加了单例模式

---
+++ DesignCode/src/main/java/com/example/qks/SingletonTest01.java
@@ -0,0 +1,16 @@
+package com.example.qks;
+
+import java.awt.*;
+
+public class SingletonTest01 {
+    public static void main(String[] args) {
+        Runtime
+    }
+}
+
+enum Singleton {
+    INSTANCE;
+    public void test() {
+        System.out.println("test");
+    }
+}
